d437110ad406303c71af080751de42180ac1537a
Roy Gans TAU Developer <roygans@mai.tau.ac.il>
2024-12-10 19:05:33 +0100

    Use wrapper functions to display respone time and accuracy of the group types in extract_behav_eeg_analysis.py

--- extract_behav_eeg_analysis.py
+++ extract_behav_eeg_analysis.py
@@ -1,7 +1,9 @@
 import pandas as pd
 import numpy as np
+from matplotlib import pyplot as plt
 
 import config
+from graph_generator import GraphGenerator
 
 def load_data_from_csv(path: str) -> pd.DataFrame:
     data = pd.read_csv(path)
@@ -12,29 +14,25 @@ def extract_data(data: pd.DataFrame, cols: list) -> pd.DataFrame:
     data = data[cols]
     return data
 
-def get_target_trials(data: pd.DataFrame, target_col_name: str, target_col_val: str) -> pd.DataFrame:
-
-    return data[data[target_col_name] == target_col_val]
-
-def get_accuracy(data_url: str) -> float:
+def get_accuracy(data_url: str, filter_func: callable) -> float:
     data = pd.read_csv(data_url)
     data = data[['is_target', 'keyResponseStimuliOnset.keys']]
     data = data.dropna(how='all')
 
     # print(data)
-    correct_trials = data[((data['is_target'] == False) & (data['keyResponseStimuliOnset.keys'].isna())) |
-                           ((data['is_target'] == True) & (data['keyResponseStimuliOnset.keys'] == 'space'))]
+    correct_trials = filter_df(data, filter_func)
     print(f'Number of correct trials: {len(correct_trials)} / {len(data)}')
     accuracy = len(correct_trials) / len(data)
     return accuracy
 
 def get_rt_metrics(data_url: str, rt_col_name: str) -> pd.DataFrame:
     data = pd.read_csv(data_url)
-    target_trials_df = get_target_trials(data, 'is_target', True)
+
+    target_trials_only_df = filter_df(data, lambda x: x['is_target'] == True)
 
     # print(f'Number of target trials: {len(target_trials_df)}')
 
-    response_time = target_trials_df[rt_col_name]
+    response_time = target_trials_only_df[rt_col_name]
 
     # Remove NaN values (Only calculate response time for target stimuli trials)
     response_time = response_time.dropna()
@@ -50,47 +48,82 @@ def get_group_rt(csv_group_path_prefix: str, num_of_participants: int) -> list:
     for i in range(1, num_of_participants + 1):
         csv_path = csv_group_path_prefix.format(i=i)
         mean_rt, std_rt = get_rt_metrics(csv_path, 'keyResponseStimuliOnset.rt')
-        rt_list.append(mean_rt)
+        rt_list.append((mean_rt, std_rt))
     return rt_list
 
+def get_group_accuracies(csv_group_path_prefix: str, num_of_participants: int) -> list:
+    accuracy_list = []
+    for i in range(1, num_of_participants + 1):
+        csv_path_participant = csv_group_path_prefix.format(i=i)
+        accuracy_of_participant = get_accuracy(csv_path_participant, lambda x: ((x['is_target'] == False) & (x['keyResponseStimuliOnset.keys'].isna())) |
+                        ((x['is_target'] == True) & (x['keyResponseStimuliOnset.keys'] == 'space')))
+        accuracy_list.append(accuracy_of_participant)
+            
+    return accuracy_list
+
+def plot_rt_distribution(response_times: list, trial_indices: int):
+    # Create plot
+    # plt.scatter(trial_indices, response_times, color='blue', alpha=0.7, label='Response Times')
+
+    plt.plot(trial_indices, response_times, color='red', linestyle='-', linewidth=2, label='Trend Line')
+
+
+    # Add labels and title
+    plt.xlabel('Trial Index', fontsize=12)
+    plt.ylabel('Response Time (s)', fontsize=12)
+    plt.title('Response Times Over Trials', fontsize=14)
+
+    # Add gridlines
+    plt.grid(alpha=0.3)
+
+    # Add legend
+    plt.legend()
+
+    # Show plot
+    plt.show()
+
+def filter_df(data: pd.DataFrame, filter_func: callable) -> pd.DataFrame:
+    return data[filter_func(data)]
 
 if __name__ == '__main__':
 
     manipulation_rt = get_group_rt(config.MANIP_CSV_PATH_PREFIX, config.NUM_OF_PARTICIPANTS_PER_GROUP)
     no_manipulation_rt = get_group_rt(config.NO_MANIP_CSV_PATH_PREFIX, config.NUM_OF_PARTICIPANTS_PER_GROUP)
 
-    print(f'Mean response time for manipulated group: {np.mean(manipulation_rt)}')
-    print(f'Mean response time for non-manipulated group: {np.mean(no_manipulation_rt)}')
+    print(f'Mean response time for manipulated group: {np.mean(manipulation_rt[0]):.3f}')
+    print(f'Mean response time for non-manipulated group: {np.mean(no_manipulation_rt[0]):.3f}')
 
+    # GraphGenerator.plot_bar_chart([f'Participant {i}' for i in range(1, config.NUM_OF_PARTICIPANTS_PER_GROUP + 1)], manipulation_rt, 'Participant', 'Mean Response Time (s)', 'blue', 'Manipulated Group')
 
-    # # Calculate accuracy
-    # data = pd.read_csv('data/meditation/manip1.csv')
-    # data = data[['is_target', 'keyResponseStimuliOnset.keys']]
-    # print(data)
-    # correct_trials = data[((data['is_target'] == False) & (data['keyResponseStimuliOnset.keys'].isna())) |
-    #                        ((data['is_target'] == True) & (data['keyResponseStimuliOnset.keys'] == 'space'))]
-    # print(correct_trials)
-    # accuracy = len(correct_trials) / len(data)
-    # print(f'Accuracy: {accuracy}')
+    manip_accs = get_group_accuracies(config.MANIP_CSV_PATH_PREFIX, config.NUM_OF_PARTICIPANTS_PER_GROUP)
+    no_manip_accs = get_group_accuracies(config.NO_MANIP_CSV_PATH_PREFIX, config.NUM_OF_PARTICIPANTS_PER_GROUP)
 
-    print('\n')
+    print(f'Mean accuracy for manipulated group: {np.mean(manip_accs):.3f}')
+    print(f'Mean accuracy for non-manipulated group: {np.mean(no_manip_accs):.3f}')
 
-    accuracy_list = []
-    no_accuracy_list = []
+    mean_rt_per_participant_manip = [rt_data[0] for rt_data in manipulation_rt]
+    std_rt_per_participant_manip = [rt_data[1] for rt_data in manipulation_rt]
 
-    for i in range(1, config.NUM_OF_PARTICIPANTS_PER_GROUP + 1):
-            csv_path = config.MANIP_CSV_PATH_PREFIX.format(i=i)
-            accuracy = get_accuracy(csv_path)
-            accuracy_list.append(accuracy)
+    mean_rt_per_participant_no_manip = [rt_data[0] for rt_data in no_manipulation_rt]
+    std_rt_per_participant_no_manip = [rt_data[1] for rt_data in no_manipulation_rt]
+
+    mean_rt_manip = np.mean(mean_rt_per_participant_manip)
+    mean_rt_no_manip = np.mean(mean_rt_per_participant_no_manip)
+
+    mean_std_rt_manip = np.mean(std_rt_per_participant_manip)
+    mean_std_rt_no_manip = np.mean(std_rt_per_participant_no_manip)
+    print(mean_std_rt_manip, mean_std_rt_no_manip)
+
+    GraphGenerator.plot_bar_chart(['Meditation', 'Control'], [mean_rt_manip, mean_rt_no_manip], [mean_std_rt_manip, mean_std_rt_no_manip], 'Group Type', 'Respone Time', 'blue', 'Manipulated Group')
     
-    print(f'Mean accuracy for manipulated group: {np.mean(accuracy_list)}')
 
-    for i in range(1, config.NUM_OF_PARTICIPANTS_PER_GROUP + 1):
-            csv_path = config.NO_MANIP_CSV_PATH_PREFIX.format(i=i)
-            accuracy = get_accuracy(csv_path)
-            no_accuracy_list.append(accuracy)
-    
-    print(f'Mean accuracy for no manipulated group: {np.mean(no_accuracy_list)}')
-    
+    mean_acc_manip = np.mean(manip_accs)
+    std_acc_manip = np.std(manip_accs)
+
+    mean_acc_no_manip = np.mean(no_manip_accs)
+    std_acc_no_manip = np.std(no_manip_accs)
+
+    GraphGenerator.plot_bar_chart(['Meditation', 'Control'], [mean_acc_manip, mean_acc_no_manip], [std_acc_manip, std_acc_no_manip], 'Group Type', 'Accuracy', 'blue', 'Manipulated Group')
+
     # Extract data
-    cols_to_extract = ['is_target', 'keyResponseStimuliOnset.keys', 'keyResponseStimuliOnset.rt']
+    # cols_to_extract = ['is_target', 'keyResponseStimuliOnset.keys', 'keyResponseStimuliOnset.rt']
